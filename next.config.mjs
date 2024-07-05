69a0a89523e12f9ded6bcca048e16b2b9a3196e9
dcolc <dcolmenares@shokworks.io>
2024-07-05 01:38:00 -0600

    image route

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/claro-video-maqueta"
 };
 
 export default nextConfig;
--- src/common/constants.ts
+++ src/common/constants.ts
@@ -2,21 +2,21 @@ export const LIST_ITEMS = [
   {
     id: 'fredVDoCUAU',
     title: 'team latam',
-    img: './assets/images/team-latam.png'
+    img: '../../public/assets/images/team-latam.png'
   },
   {
     id: 'UomdFp8DNys',
     title: 'travel meets fashion',
-    img: './assets/images/travel-meets-fashion.png'
+    img: '../../public/assets/images/travel-meets-fashion.png'
   },
   {
     id: 'X79TnHzPn2M',
     title: 'velocidad y riesgo',
-    img: './assets/images/velocidad-riesgo.png'
+    img: '../../public/assets/images/velocidad-riesgo.png'
   },
   {
     id: 'oWdyldXwLRk',
     title: 'experiencia multimedia',
-    img: './assets/images/experiencia.png'
+    img: '../../public/assets/images/experiencia.png'
   }
 ];
